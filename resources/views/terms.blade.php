6c9ec0b601cc617a4f814cd77ff720ebc8f3fa0b
marconarca <narcamarco@gmail.com>
2024-03-06 00:14:30 +0800

    init commit

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.layout')
+
+@section('content')
+
+<h1>Term</h1>
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum excepturi, enim itaque culpa sint alias. Corporis labore et mollitia quasi doloribus perferendis cupiditate velit sunt autem qui. Incidunt quisquam nobis odio at ex suscipit sint aut fugit. Quis est expedita nisi ullam illo id. Cupiditate error repellat illum aspernatur nostrum.
+</div>
+
+@endsection
